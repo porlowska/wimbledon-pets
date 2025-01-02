2d164845d95f42d1cbe03e60cc34d132ab7fb843
Polly Orlowska <pollyorlowska@192.168.1.113>
2025-01-02 11:52:14 +0000

    fix: added heading to all services page

--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -6,7 +6,12 @@ const Services = () => {
   return (
     <>
       <ServiceLayout service={services[0]} />
-      <ServicesSection/>
+      <div className="pb-24">
+        <h4 className="mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 py-4  text-pretty text-4xl font-semibold tracking-tight text-secondary sm:text-5xl ">
+          Expore our services:
+        </h4>
+        <ServicesSection />
+      </div>
     </>
   );
 };
