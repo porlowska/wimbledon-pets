9d8f536783ebc243d1c3ebd24f8b881033ad35a0
Polly Orlowska <pollyorlowska@Pollys-Book.local>
2025-01-13 10:50:23 +0100

    form

--- app/_components/form/Form.tsx
+++ app/_components/form/Form.tsx
@@ -1,16 +1,44 @@
+"use client";
+import { useState } from "react";
 import Button from "@/app/_components/Button";
 import Input from "@/app/_components/form/Input";
 import Textarea from "@/app/_components/form/Textarea";
+import { contactFormSchema } from "@/app/_utils/contactForm.validation.utils";
+import toast from "react-hot-toast";
+
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    ownerName: "",
+    petName: "",
+    email: "",
+    phone: "",
+    postcode: "",
+    message: "",
+  });
+
+  function handleSubmit(e: any) {
+    e.preventDeafult;
+  }
+
   return (
     <form
       name="book-now"
       className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
     >
-      <input type="hidden" name="book-now" value="book-now" />
       <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <Input inputLabel={"Your Name"} type={"text"} required />
+          <Input
+            inputLabel={"Your Name"}
+            type={"text"}
+            required
+            value={formData.ownerName}
+            onChange={function (e: React.ChangeEvent<HTMLInputElement>): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
           <Input inputLabel={"Pet(s) name"} type={"text"} required />
 
           <div className="sm:col-span-2">
--- app/api/book-now/route.ts
+++ app/api/book-now/route.ts
@@ -2,33 +2,8 @@ import handleError from "@/app/_utils/error.utils";
 import { sendEmail } from "@/app/_utils/sendGrid.utils";
 import handleServerError from "@/app/_utils/serverErr.utils";
 import { NextResponse } from "next/server";
-import * as yup from "yup";
+import { contactFormSchema } from "@/app/_utils/contactForm.validation.utils";
 
-const contactFormSchema = yup.object().shape({
-  ownerName: yup
-    .string()
-    .min(2, "Must be at least 2 characters")
-    .max(50, "Must be at most 50 characters")
-    .required("Your name is required"),
-  petName: yup
-    .string()
-    .min(2, "Must be at least 2 characters")
-    .max(20, "Must be at most 20 characters")
-    .required("Last name is required"),
-  email: yup
-    .string()
-    .email("Invalid email format")
-    .required("Email is required"),
-  phone: yup
-    .string()
-    .matches(/^\+?[0-9]{6,15}$/, "Phone number must be between 6 and 15 digits"),
-  postcode: yup
-    .string()
-    .required("Postcode is required")
-    .min(3, "Must be at least 3 characters")
-    .max(10, "Must be at most 10 characters"),
-  message: yup.string().min(3, "Message must be at least 3 characters"),
-});
 
 export async function POST(req: Request) {
   try {
